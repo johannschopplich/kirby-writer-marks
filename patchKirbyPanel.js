e0a54a8c3d47744d91ab3cf239a0e26699eaf05c
Johann Schopplich <mail@johannschopplich.com>
2021-03-21 21:31:45 +0100

    chore: add custom marks to end of toolbar list

--- patchKirbyPanel.js
+++ patchKirbyPanel.js
@@ -3,8 +3,8 @@ import fs from 'fs'
 // Extend writer field to support custom inline marks
 const path = 'kirby/panel/dist/js/app.js'
 const build = fs.readFileSync(path, 'utf8')
-const pattern = 'createMarks(){return this.filterExtensions({'
-const patch = pattern + '...window.panel.plugins.writer||{},'
+const pattern = '},this.marks)}'
+const patch = ',...window.panel.plugins.writer||{}' + pattern
 
 if (!build.includes(patch)) {
   console.log('Patching Kirby Panel to support custom writer plugins')
