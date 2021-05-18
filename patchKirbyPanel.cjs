7f9d90231b64ddc2976d8e9333d744db5fac178c
Johann Schopplich <mail@johannschopplich.com>
2021-05-18 14:19:39 +0200

    Support public folder setup

--- patchKirbyPanel.cjs
+++ patchKirbyPanel.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 // Extend writer field to support custom inline marks
-const path = 'kirby/panel/dist/js/app.js'
+const path = (fs.existsSync('vendor/getkirby') ? 'vendor/getkirby/cms' : 'kirby') + '/panel/dist/js/app.js'
 const build = fs.readFileSync(path, 'utf8')
 const pattern = '},this.marks)}'
 const patch = ',...(\'marksResolver\' in window.panel.plugins?window.panel.plugins.marksResolver():{})' + pattern
